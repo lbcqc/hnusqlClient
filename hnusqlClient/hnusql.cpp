de61047b1f6f8b4e4a6d3437b19cea8df4ff5ef7
帅 李 <981916645@qq.com>
2017-07-05 17:35:59 +0800

    正常更新

--- hnusqlClient/hnusql.cpp
+++ hnusqlClient/hnusql.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <mysql.h>
 using namespace std;
 int main(int argc, char* args[])
 {
-	cout << "hello hnusql" << endl;
+	/*连接程序-begin*/
+	//此处处理命令行参数，执行初始化及连接操作
+
+
+	/*连接程序-end*/
+
+	/*读取命令并执行-begin*/
+	//此处读取sql语句并执行
+	for (;;)
+	{
+		
+	}
+	/*读取命令并执行-end*/
 	return 0;
 }
